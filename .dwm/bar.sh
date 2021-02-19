2f4d4f24562be9f00120f6f21d7c29a2e351d703
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-02-18 21:07:37 -0600

    added ram, cpu, and swap info

--- .dwm/bar.sh
+++ .dwm/bar.sh
@@ -1,9 +1,22 @@
+#!/bin/bash
+
 dte() {
-	dte="$(date +"%A, %B %d - %I:%M %p")"
-	echo -e "$dte"
+    echo "$(date +"%A, %B %d - %I:%M %p ")"
+}
+
+cpu() {
+    printf "%.2f%%" "$(awk '/cpu / {print ($2+$4)*100/($2+$4+$5)}' /proc/stat)"
+}
+
+ram() {
+    printf "%.2f%%" "$(free -m | awk '/Mem/ {print ($3/$2)*100}')"
+}
+
+swp() {
+    printf "%.2f%%" "$(free -m | awk '/Swap/ {print ($3/$2)*100}')"
 }
 
 while true; do
-	xsetroot -name "$(dte)"
-	sleep 30s
+    xsetroot -name "ram: $(ram) swap: $(swp) cpu: $(cpu) $(dte)"
+    sleep 30s
 done
